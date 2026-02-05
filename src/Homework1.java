18c797d0b42d5ecca23a0ada3500e0ba9bebf4ca
bvlh <brandonh15527@gmail.com>
2026-02-05 02:47:48 -0800

    Fixed comments

--- src/Homework1.java
+++ src/Homework1.java
@@ -2,36 +2,38 @@ import java.util.Arrays;
 
 public class Homework1 {
     public static int[] mergeTwoSortedArr(int[] a, int[] b) {
-        int[] result = new int[a.length + b.length];    // max length of new array must be 
-                                                        // length of first two arrays combined
+        // max size of new array must be length of first two arrays combined
+        int[] result = new int[a.length + b.length];    
         int i = 0;                              // pointer for array a
         int j = 0;                              // pointer for array b
-        int k = 0;                              // pointer for new array
+        int k = 0;                              // pointer for new sorted array
 
         while (i < a.length && j < b.length) {
-            if (a[i] < b[j]) {                  // if b[j] is greater
+            if (a[i] < b[j]) {                  // if a[i] is smaller
                 result[k] = a[i];               // new array adds a[i]
                 i++;
             } else {                            // if a[i] is greater
-                result[k] = b[j];
-                j++;                            // new array adds b[i]
+                result[k] = b[j];               // new array adds b[j]
+                j++;                        
             }
             k++;
         }
 
-        while (i < a.length) {                  // no more elemnts in b, copy rest of a
+        // If no more elements in b, copy rest of a
+        while (i < a.length) {                  
             result[k] = a[i];
             i++;
             k++;
         }
 
-        while (j < b.length) {                  // no more elements in a, copy rest of b
+        // If no more elements in a, copy rest of b
+        while (j < b.length) {                  
             result[k] = b[j];
             j++;
             k++;
         }
 
-        return result;                          // return new merged array
+        return result;                          // return merged sorted array
     }
 
     public static void main(String[] args) {
